43a1ffbdd8dfb82b0776934a8eb37f4d7a436320
Mouli Uddanti <uddanti.mouli91@gmail.com>
2018-02-07 23:35:22 +0530

    Create Bank.sol

---
+++ Bank.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.4.18;
+contract Bank {
+    address owner;
+    uint amount;
+
+function Bank() public {
+    owner = msg.sender;
+    amount = 0;
+    }
+
+function depositAmount() payable returns(uint) {
+    if(msg.value >0)
+       amount = amount + 1;
+       return numberOfTimesAmountDeposited();
+    }
+
+function numberOfTimesAmountDeposited() constant returns (uint) {
+    return amount;
+    }
+
+function kill(){
+    if(msg.sender == owner)
+    selfdestruct(owner); 
+    }
+}
+  
